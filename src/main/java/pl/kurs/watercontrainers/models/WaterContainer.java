6a4590cfa01475dea9b5197dd6923d9e20bfb9bf
mrsebo24 <sebastian.wiki@wp.pl>
2025-06-08 11:41:41 +0200

    added WaterContainer model

---
+++ src/main/java/pl/kurs/watercontrainers/models/WaterContainer.java
@@ -0,0 +1,68 @@
+package pl.kurs.watercontrainers.models;
+
+import java.util.Objects;
+
+public class WaterContainer {
+
+    private String name;
+    private double maxCapacity;
+    private double waterLevel;
+
+    public WaterContainer(String name, double maxCapacity, double waterLevel) {
+        this.name = name;
+        this.maxCapacity = maxCapacity;
+        this.waterLevel = waterLevel;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getMaxCapacity() {
+        return maxCapacity;
+    }
+
+    public void setMaxCapacity(double maxCapacity) {
+        this.maxCapacity = maxCapacity;
+    }
+
+    public double getWaterLevel() {
+        return waterLevel;
+    }
+
+    public void setWaterLevel(double waterLevel) {
+        this.waterLevel = waterLevel;
+    }
+
+
+
+
+
+
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        WaterContainer that = (WaterContainer) o;
+        return Double.compare(maxCapacity, that.maxCapacity) == 0 && Double.compare(waterLevel, that.waterLevel) == 0 && Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, maxCapacity, waterLevel);
+    }
+
+    @Override
+    public String toString() {
+        return "WaterContainer{" +
+                "name='" + name + '\'' +
+                ", maxCapacity=" + maxCapacity +
+                ", waterLevel=" + waterLevel +
+                '}';
+    }
+}
