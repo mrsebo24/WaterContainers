ccce341487ec6e6495ec82e81bcb2a5d13b9c06b
Michal Sloniewski <michal.sloniewski@smartcodecreations.com>
2025-06-17 18:21:58 +0200

    added Car model

---
+++ src/main/java/pl/kurs/watercontrainers/models/Car.java
@@ -0,0 +1,41 @@
+package pl.kurs.watercontrainers.models;
+
+import java.io.Serializable;
+
+public class Car {
+
+
+
+    private String producer;
+    private String model;
+
+    public Car(String producer, String model) {
+        this.producer = producer;
+        this.model = model;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "producer='" + producer + '\'' +
+                ", model='" + model + '\'' +
+                '}';
+    }
+
+}
