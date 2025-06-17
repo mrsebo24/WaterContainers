4c5cb50a5c377e938618552c4d922fcb3389efea
michalsloniewski <111369173+michalsloniewski@users.noreply.github.com>
2025-06-17 18:56:05 +0200

    Feature/task 2 car model (#3)
    * added Car model
    
    * added serializable to Car model
    
    * added serialVersionUID and equals/hashcode methods

---
+++ src/main/java/pl/kurs/watercontrainers/models/Car.java
@@ -0,0 +1,57 @@
+package pl.kurs.watercontrainers.models;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Car implements Serializable {
+    private static final long serialVersionUID = 42L;
+
+
+
+
+    private String producer;
+    private String model;
+
+    public Car(String producer, String model) {
+        this.producer = producer;
+        this.model = model;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return Objects.equals(producer, car.producer) && Objects.equals(model, car.model);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(producer, model);
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "producer='" + producer + '\'' +
+                ", model='" + model + '\'' +
+                '}';
+    }
+
+}
