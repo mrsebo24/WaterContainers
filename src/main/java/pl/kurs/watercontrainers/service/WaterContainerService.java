5e412aa340bc3b1c13c416c35a095c590ebb022f
mrsebo24 <sebastian.wiki@wp.pl>
2025-06-20 16:49:27 +0200

    added to class WaterContainerService method findWaterContainerCapacity

--- src/main/java/pl/kurs/watercontrainers/service/WaterContainerService.java
+++ src/main/java/pl/kurs/watercontrainers/service/WaterContainerService.java
@@ -13,6 +13,16 @@ public class WaterContainerService {
     }
 
     //- znalezc zbiornik w ktorym jest najwiecej wody
+    public Optional<WaterContainer> findWaterContainerMaxCapacity(){
+        if(waterContainers.isEmpty()) return Optional.empty();
+
+        WaterContainer result = waterContainers.get(0);
+        for (WaterContainer waterContainer : waterContainers) {
+            if (waterContainer.getMaxCapacity() > result.getMaxCapacity()){
+                result = waterContainer;
+            }
+        }return Optional.of(result);
+    }
     //- znalezc zbiornik ktory jest najbardziej zapelniony
     //- znalezc wszystkie puste zbiorniki.
     //- pozwalają znaleźć zbiornik na którym było najwiecej operacji zakonczonych niepowodzeniem
