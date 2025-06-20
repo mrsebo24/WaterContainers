20c230d39795f81e48fd140dc6523dd033595a9a
mrsebo24 <sebastian.wiki@wp.pl>
2025-06-20 16:48:49 +0200

    added class WaterContainerService and create field, constructor

---
+++ src/main/java/pl/kurs/watercontrainers/service/WaterContainerService.java
@@ -0,0 +1,20 @@
+package pl.kurs.watercontrainers.service;
+
+import pl.kurs.watercontrainers.models.WaterContainer;
+
+import java.util.List;
+import java.util.Optional;
+
+public class WaterContainerService {
+    private List<WaterContainer> waterContainers;
+
+    public WaterContainerService(List<WaterContainer> waterContainers) {
+        this.waterContainers = waterContainers;
+    }
+
+    //- znalezc zbiornik w ktorym jest najwiecej wody
+    //- znalezc zbiornik ktory jest najbardziej zapelniony
+    //- znalezc wszystkie puste zbiorniki.
+    //- pozwalają znaleźć zbiornik na którym było najwiecej operacji zakonczonych niepowodzeniem
+    //- pozwalają znaleźć zbiornik w którym było najwięcej operacji danego typu (typ podajemy jako argument metody)
+}
